b7c8b64031261f4638420932c6873a9a20d374aa
Michael Petrov <petrov.michael@gmail.com>
2012-04-25 15:56:03 -0400

    Added a static library build target

---
+++ FGallery/FGallery.h
@@ -0,0 +1,10 @@
+//
+//  FGallery.h
+//  FGallery
+//
+//  Created by Michael Petrov on 12-04-25.
+//  Copyright (c) 2012 Michael Petrov. All rights reserved.
+//
+
+#import "Classes/FGalleryViewController.h"
+
